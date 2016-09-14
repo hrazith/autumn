81985de89dd5ef6121183909efc573c53dd0720c
SachinInd08 <visit.sachin08@gmail.com>
2016-09-13 23:42:07 -0400

    Added new proc

--- _db/procCustAppInfoInsert.sql
+++ _db/procCustAppInfoInsert.sql
@@ -1,20 +1,11 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `procCustAppInfoInsert`( CustProfileId INT,
-                                         CountryListId INT,
-                                         CountryVisaTypeListId INT,
-                                         CustAppNumber VARCHAR(200),
-                                         Activated ENUM('0','1'),
-                                         Completed ENUM('0','1'),
-                                         Submitted ENUM('0','1'),
-                                         CreatedOn DATETIME,
-                                         ModifiedOn DATETIME
-                                       )
+CREATE DEFINER=`root`@`localhost` PROCEDURE `procCustAppInfoInsert`(IN `CustProfileId` INT, IN `CountryListId` INT, IN `CountryVisaTypeListId` INT, IN `CustAppNumber` VARCHAR(200))
 BEGIN
  
  INSERT INTO custappinfo
- (CustProfileInfoId,CountryListId,CountryVisaTypeListId,CustAppNumber,Activated,Completed,Submitted,CreatedOn,ModifiedOn)
+ (CustProfileInfoId,CountryListId,CountryVisaTypeListId,CustAppNumber)
  VALUES
- (CustProfileId,CountryListId,CountryVisaTypeListId,CustAppNumber,Activated,Completed,Submitted,CreatedOn,ModifiedOn);
+ (CustProfileId,CountryListId,CountryVisaTypeListId,CustAppNumber);
  
  END$$
 DELIMITER ;
