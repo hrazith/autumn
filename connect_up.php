a60dd89951d88702305fc1e38b71a13b57fc2217
clavilab <carlos@artzones.com>
2016-10-24 16:39:22 -0400

    go

---
+++ connect_up.php
@@ -0,0 +1,8 @@
+<?php
+    
+    $dbCon = mysqli_connect("localhost","admin", "PorCino8321", "autumn");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect:". mysqli_connect_error();
+    }
+
+?>
