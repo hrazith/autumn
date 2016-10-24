753b6137f23066d43b85b73704b5721752fb2605
clavilab <carlos@artzones.com>
2016-10-24 17:16:22 -0400

    n

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
     
-    $dbCon = mysqli_connect("localhost","root", "root", "autumn");
+    $dbCon = mysqli_connect("localhost","admin", "PorCino8321", "autumn");
     if (mysqli_connect_errno()) {
         echo "Failed to connect:". mysqli_connect_error();
     }
